0abb6341f133401ef70c7fb1608aaa53329f92ba
GustavEikaas <gustav.eikaas@gmail.com>
2025-09-10 20:03:23 +0200

    feat: dotnet outdated qualify full path

--- EasyDotnet.Tool/Controllers/Outdated/OutdatedController.cs
+++ EasyDotnet.Tool/Controllers/Outdated/OutdatedController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using EasyDotnet.Services;
@@ -12,6 +13,7 @@ public class OutdatedController(OutdatedService oudatedService) : BaseController
   [JsonRpcMethod("outdated/packages")]
   public async Task<IAsyncEnumerable<OutdatedDependencyInfoResponse>> GetOutdatedPackages(string targetPath, bool? includeTransitive = false)
   {
+    var fullPath = Path.GetFullPath(targetPath);
     var dependencies = await oudatedService.AnalyzeProjectDependenciesAsync(
                         targetPath,
                         includeTransitive: includeTransitive ?? false,
