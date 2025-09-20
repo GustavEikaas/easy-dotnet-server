2bf264f5ae84be1bf49ad184e917b0b055bba18c
GustavEikaas <gustav.eikaas@gmail.com>
2025-09-20 23:42:47 +0200

    fix: launch profile service should work with both file path and dir path

--- EasyDotnet.Infrastructure/Services/LaunchProfileService.cs
+++ EasyDotnet.Infrastructure/Services/LaunchProfileService.cs
@@ -13,11 +13,12 @@ public class LaunchProfileService : ILaunchProfileService
 
   public Dictionary<string, LaunchProfile>? GetLaunchProfiles(string targetPath)
   {
-    var launchSettingsPath = Path.Combine(
-        targetPath,
-        "Properties",
-        "launchSettings.json"
-    );
+    var baseDir = File.Exists(targetPath) &&
+                      string.Equals(Path.GetExtension(targetPath), ".csproj", StringComparison.OrdinalIgnoreCase)
+            ? Path.GetDirectoryName(targetPath)!
+            : targetPath;
+
+    var launchSettingsPath = Path.Combine(baseDir, "Properties", "launchSettings.json");
 
     if (!File.Exists(launchSettingsPath))
     {
