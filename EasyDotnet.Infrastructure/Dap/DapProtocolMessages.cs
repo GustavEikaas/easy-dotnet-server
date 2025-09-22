09052b9c9735bbdeacb2e43c7ebc93c1cea1e740
GustavEikaas <gustav.eikaas@gmail.com>
2025-09-22 12:55:51 +0200

    fix: nullable issues

--- EasyDotnet.Infrastructure/Dap/DapProtocolMessages.cs
+++ EasyDotnet.Infrastructure/Dap/DapProtocolMessages.cs
@@ -5,38 +5,38 @@ namespace EasyDotnet.Infrastructure.Dap;
 
 public class ProtocolMessage
 {
-  public int Seq { get; set; }
-  public string Type { get; set; }
+  public required int Seq { get; set; }
+  public required string Type { get; set; }
 
   [JsonExtensionData]
-  public Dictionary<string, JsonElement> ExtraProperties { get; set; }
+  public Dictionary<string, JsonElement> ExtraProperties { get; set; } = [];
 }
 
 public class Request : ProtocolMessage
 {
-  public string Command { get; set; }
+  public required string Command { get; set; }
   public JsonElement? Arguments { get; set; }
 }
 
 public class Event : ProtocolMessage
 {
   [JsonPropertyName("event")]
-  public string EventName { get; set; }
+  public required string EventName { get; set; }
   public JsonElement? Body { get; set; }
 }
 
 public class Response : ProtocolMessage
 {
-  public int RequestSeq { get; set; }
-  public bool Success { get; set; }
-  public string Command { get; set; }
-  public string Message { get; set; }
+  public required int RequestSeq { get; set; }
+  public required bool Success { get; set; }
+  public required string Command { get; set; }
+  public string? Message { get; set; }
   public JsonElement? Body { get; set; }
 }
 
 public class ErrorResponse : Response
 {
-  public new ErrorBody Body { get; set; }
+  public new required ErrorBody Body { get; set; }
 }
 
 public class ErrorBody
@@ -44,5 +44,5 @@ public class ErrorBody
   public JsonElement? Error { get; set; }
 
   [JsonExtensionData]
-  public Dictionary<string, JsonElement> ExtraProperties { get; set; }
+  public Dictionary<string, JsonElement>? ExtraProperties { get; set; } = [];
 }
