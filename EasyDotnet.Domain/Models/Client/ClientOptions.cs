ce1456f38c1c3821f74ffcb5d505799553e8aeb4
GustavEikaas <gustav.eikaas@gmail.com>
2025-09-29 21:20:14 +0200

    .

--- EasyDotnet.Domain/Models/Client/ClientOptions.cs
+++ EasyDotnet.Domain/Models/Client/ClientOptions.cs
@@ -1,4 +1,4 @@
 namespace EasyDotnet.Domain.Models.Client;
 
 public sealed record DebuggerOptions(string? BinaryPath = null);
-public sealed record ClientOptions(DebuggerOptions? DebuggerOptions = null, bool UseVisualStudio = false, bool UseRoslynLsp = false);
+public sealed record ClientOptions(DebuggerOptions? DebuggerOptions = null, bool UseVisualStudio = false);
