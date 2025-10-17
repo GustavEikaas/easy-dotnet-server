9104c2ef7ef4cf8dc12fa40f2cde7c47b3400f7f
GustavEikaas <gustav.eikaas@gmail.com>
2025-10-17 09:14:12 +0200

    .

--- EasyDotnet.Infrastructure/Services/ClientService.cs
+++ EasyDotnet.Infrastructure/Services/ClientService.cs
@@ -1,9 +1,10 @@
 using EasyDotnet.Application.Interfaces;
 using EasyDotnet.Domain.Models.Client;
+using StreamJsonRpc;
 
 namespace EasyDotnet.Infrastructure.Services;
 
-public class ClientService : IClientService
+public class ClientService(JsonRpc rpc) : IClientService
 {
   public bool IsInitialized { get; set; }
   public bool UseVisualStudio { get; set; } = false;
@@ -18,4 +19,7 @@ public class ClientService : IClientService
       throw new Exception("Client has not initialized yet");
     }
   }
+
+  public async Task<bool> RequestOpenBuffer(string path) => await rpc.InvokeWithParameterObjectAsync<bool>("openBuffer", new { path });
+  public async Task<bool> RequestSetBreakpoint(string path, int lineNumber) => await rpc.InvokeWithParameterObjectAsync<bool>("setBreakpoint", new { path, lineNumber, });
 }
