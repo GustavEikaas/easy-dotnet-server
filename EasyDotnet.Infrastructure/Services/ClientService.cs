8bd11a975ce3b5a9de77556c5ca6e9e37cb03a8a
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2025-10-17 09:25:47 +0200

    feat(RPC): client methods `openBuffer`, `setBreakpoint` (#160)

--- EasyDotnet.Infrastructure/Services/ClientService.cs
+++ EasyDotnet.Infrastructure/Services/ClientService.cs
@@ -1,9 +1,13 @@
 using EasyDotnet.Application.Interfaces;
 using EasyDotnet.Domain.Models.Client;
+using StreamJsonRpc;
 
 namespace EasyDotnet.Infrastructure.Services;
 
-public class ClientService : IClientService
+public sealed record SetBreakpointRequest(string Path, int LineNumber);
+public sealed record OpenBufferRequest(string Path);
+
+public class ClientService(JsonRpc rpc) : IClientService
 {
   public bool IsInitialized { get; set; }
   public bool UseVisualStudio { get; set; } = false;
@@ -18,4 +22,7 @@ public class ClientService : IClientService
       throw new Exception("Client has not initialized yet");
     }
   }
+
+  public async Task<bool> RequestOpenBuffer(string path) => await rpc.InvokeWithParameterObjectAsync<bool>("openBuffer", new OpenBufferRequest(path));
+  public async Task<bool> RequestSetBreakpoint(string path, int lineNumber) => await rpc.InvokeWithParameterObjectAsync<bool>("setBreakpoint", new SetBreakpointRequest(path, lineNumber));
 }
