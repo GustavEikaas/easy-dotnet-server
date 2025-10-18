2a9646b6fa71f73d92bc6991834bdd2e4b00db08
GustavEikaas <gustav.eikaas@gmail.com>
2025-10-18 18:56:13 +0200

    feat(roslyn): minimum sdk warning

--- EasyDotnet.IDE/Controllers/Lsp/LspController.cs
+++ EasyDotnet.IDE/Controllers/Lsp/LspController.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
+using EasyDotnet.Application.Interfaces;
 using EasyDotnet.Controllers;
 using EasyDotnet.IDE.Utils;
 using Microsoft.Extensions.Logging;
@@ -7,12 +8,16 @@ using StreamJsonRpc;
 
 namespace EasyDotnet.IDE.Controllers.Lsp;
 
-public class LspController(ILogger<LspController> logger) : BaseController
+public class LspController(IMsBuildService msBuildService, ILogger<LspController> logger) : BaseController
 {
   public sealed record LspStartResponse(string Pipe);
   [JsonRpcMethod("lsp/start")]
   public async Task<LspStartResponse> StartLsp(bool useRoslynator = true, string[]? analyzerAssemblies = null, CancellationToken cancellationToken = default)
   {
+    if (!msBuildService.HasMinimumSdk(new System.Version(9, 0)))
+    {
+      throw new System.Exception("Roslyn LSP requires .net 9 sdk or higher");
+    }
     var pipeName = PipeUtils.GeneratePipeName();
     var proxy = new RoslynProxy(pipeName, logger);
 
