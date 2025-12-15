c857bd87388ecf92b6f1cb5b3ad7082366194aa2
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2025-12-15 11:35:41 +0100

    feat(debugger): add explicit error for debugging unsupported platform TFMs (#218)

--- EasyDotnet.MsBuild/DotnetProject.cs
+++ EasyDotnet.MsBuild/DotnetProject.cs
@@ -149,3 +149,58 @@ public record DotnetProject
     string? UserProfileRuntimeStorePath,
     string? TargetPlatformIdentifier
 );
+
+public static class DotnetProjectTfmExtensions
+{
+  public static string? GetTfmPlatform(this DotnetProject project)
+  {
+    var tfm = project.TargetFramework;
+    if (string.IsNullOrWhiteSpace(tfm))
+      return null;
+
+    var dashIndex = tfm.IndexOf('-');
+    if (dashIndex < 0 || dashIndex == tfm.Length - 1)
+    {
+      return null;
+    }
+    return tfm[(dashIndex + 1)..];
+  }
+}
+
+public static class DotnetProjectPlatformExtensions
+{
+
+  public static DotnetPlatform GetPlatform(this DotnetProject project)
+  {
+    var platform = project.GetTfmPlatform()?.ToLowerInvariant();
+
+    return platform switch
+    {
+      null => DotnetPlatform.None,
+      var p when p.StartsWith("android") => DotnetPlatform.Android,
+      var p when p.StartsWith("ios") => DotnetPlatform.iOS,
+      var p when p.StartsWith("maccatalyst") => DotnetPlatform.MacCatalyst,
+      var p when p.StartsWith("macos") => DotnetPlatform.MacOS,
+      var p when p.StartsWith("tvos") => DotnetPlatform.TvOS,
+      var p when p.StartsWith("tizen") => DotnetPlatform.Tizen,
+      var p when p.StartsWith("browser") => DotnetPlatform.Browser,
+      var p when p.StartsWith("windows") => DotnetPlatform.Windows,
+      _ => DotnetPlatform.Unknown
+    };
+  }
+
+}
+
+public enum DotnetPlatform
+{
+  None,
+  Android,
+  iOS,
+  MacCatalyst,
+  MacOS,
+  TvOS,
+  Tizen,
+  Browser,
+  Windows,
+  Unknown
+}
