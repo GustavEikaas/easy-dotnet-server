57913855cb41b1c53f9467920d0e92e6d12f0237
Gustav Eikaas <gustav.eikaas@rotorsystems.no>
2026-01-14 09:10:31 +0100

    feat: client picker v2

--- EasyDotnet.Domain/Models/Client/ClientInfo.cs
+++ EasyDotnet.Domain/Models/Client/ClientInfo.cs
@@ -1,3 +1,7 @@
 namespace EasyDotnet.Domain.Models.Client;
 
 public sealed record ClientInfo(string Name, string? Version);
+
+public sealed record ClientCapabilities(PickerCapabilities PickerCapabilities);
+
+public sealed record PickerCapabilities(bool SupportsAutoCancel);
