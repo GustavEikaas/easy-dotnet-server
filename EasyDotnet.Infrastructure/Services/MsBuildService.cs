b219dad1df3f6e5592a6024a8c3781251158c851
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2026-02-03 08:40:05 +0100

    feat(msbuild): run restore task before build (#260)

--- EasyDotnet.Infrastructure/Services/MsBuildService.cs
+++ EasyDotnet.Infrastructure/Services/MsBuildService.cs
@@ -54,7 +54,7 @@ public class MsBuildService(IVisualStudioLocator locator, IClientService clientS
       throw new ArgumentException("Target path must be provided", nameof(targetPath));
     }
 
-    var (command, args) = await GetCommandAndArguments(clientService.UseVisualStudio ? MSBuildProjectType.VisualStudio : MSBuildProjectType.SDK, targetPath, targetFrameworkMoniker, configuration, buildArgs);
+    var (command, args) = await GetBuildAndRestoreCommandAndArguments(clientService.UseVisualStudio ? MSBuildProjectType.VisualStudio : MSBuildProjectType.SDK, targetPath, targetFrameworkMoniker, configuration, buildArgs);
 
     var (success, stdout, stderr) = await processQueue.RunProcessAsync(command, args, new ProcessOptions(true), cancellationToken);
 
@@ -386,6 +386,26 @@ public class MsBuildService(IVisualStudioLocator locator, IClientService clientS
     {
       MSBuildProjectType.SDK => ("dotnet", $"msbuild \"{targetPath}\" {config} {tfmArg} {args ?? ""}"),
       MSBuildProjectType.VisualStudio => (await locator.GetVisualStudioMSBuildPath(), $"\"{targetPath}\" {config} {tfmArg} {args ?? ""}"),
+      _ => throw new InvalidOperationException("Unknown MSBuild type")
+    };
+  }
+
+  private async Task<(string Command, string Arguments)> GetBuildAndRestoreCommandAndArguments(
+      MSBuildProjectType type,
+      string targetPath,
+      string? targetFrameworkMoniker,
+      string? configuration, string? args)
+  {
+    var tfmArg = string.IsNullOrWhiteSpace(targetFrameworkMoniker)
+        ? string.Empty
+        : $" /p:TargetFramework={targetFrameworkMoniker}";
+
+    var config = string.IsNullOrEmpty(configuration) ? "" : $"/p:Configuration={configuration}";
+
+    return type switch
+    {
+      MSBuildProjectType.SDK => ("dotnet", $"msbuild \"{targetPath}\" {config} {tfmArg} /t:restore /t:build {args ?? ""}"),
+      MSBuildProjectType.VisualStudio => (await locator.GetVisualStudioMSBuildPath(), $"\"{targetPath}\" {config} {tfmArg} /t:restore /t:build {args ?? ""}"),
       _ => throw new InvalidOperationException("Unknown MSBuild type")
     };
   }
