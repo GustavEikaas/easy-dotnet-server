475ecb573769bc08042524afd055e5ebbe51df41
Gustav Eikaas <gustav.eikaas@gmail.com>
2026-02-17 09:58:50 +0100

    feat: add linq async extensions

--- EasyDotnet.IDE/BuildHost/BuildHostManager.cs
+++ EasyDotnet.IDE/BuildHost/BuildHostManager.cs
@@ -135,3 +135,26 @@ public sealed class BuildHostManager(ILogger<BuildHostManager> logger, BuildHost
     await InvalidateConnectionAsync();
   }
 }
+
+public static class AsyncEnumerableExtensions
+{
+  public static async Task<List<T>> ToListAsync<T>(
+      this IAsyncEnumerable<T> source,
+      CancellationToken cancellationToken)
+  {
+    var list = new List<T>();
+    await foreach (var item in source.WithCancellation(cancellationToken))
+    {
+      list.Add(item);
+    }
+    return list;
+  }
+
+  public static async Task<List<T>> ToListAsync<T>(
+      this Task<IAsyncEnumerable<T>> sourceTask,
+      CancellationToken cancellationToken)
+  {
+    var source = await sourceTask.ConfigureAwait(false);
+    return await source.ToListAsync(cancellationToken).ConfigureAwait(false);
+  }
+}
