dc63733c22146985e0b847c4bcb9b32bae22187c
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2026-02-22 15:20:19 +0100

    feat(vstest): async stream test run results (#292)

---
+++ EasyDotnet.IDE/VSTest/TestRunHandler.cs
@@ -0,0 +1,39 @@
+using System.Threading.Channels;
+using EasyDotnet.Types;
+using EasyDotnet.VSTest;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace EasyDotnet.IDE.VSTest;
+
+internal sealed class TestRunHandler(ChannelWriter<TestRunResult> writer) : ITestRunEventsHandler
+{
+  public void HandleLogMessage(TestMessageLevel level, string? message) { }
+
+  public void HandleRawMessage(string rawMessage) { }
+
+  public void HandleTestRunComplete(TestRunCompleteEventArgs testRunCompleteArgs, TestRunChangedEventArgs? lastChunkArgs, ICollection<AttachmentSet>? runContextAttachments, ICollection<string>? executorUris)
+  {
+    if (lastChunkArgs?.NewTestResults is not null)
+    {
+      foreach (var result in lastChunkArgs.NewTestResults)
+      {
+        writer.TryWrite(result.ToTestRunResult());
+      }
+    }
+  }
+
+  public void HandleTestRunStatsChange(TestRunChangedEventArgs? testRunChangedArgs)
+  {
+    if (testRunChangedArgs?.NewTestResults is not null)
+    {
+      foreach (var result in testRunChangedArgs.NewTestResults)
+      {
+        writer.TryWrite(result.ToTestRunResult());
+      }
+    }
+  }
+
+  public int LaunchProcessWithDebuggerAttached(TestProcessStartInfo testProcessStartInfo) => throw new NotImplementedException();
+}
