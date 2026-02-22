67d3f3055e72f2d47bf898b20f775caf38e6f672
Gustav Eikaas <gustav.eikaas@gmail.com>
2026-02-22 20:50:42 +0100

    feat: add external terminal client option

--- EasyDotnet.Domain/Models/Client/ClientOptions.cs
+++ EasyDotnet.Domain/Models/Client/ClientOptions.cs
@@ -1,4 +1,12 @@
 namespace EasyDotnet.Domain.Models.Client;
 
 public sealed record DebuggerOptions(string? BinaryPath = null, bool ApplyValueConverters = false);
-public sealed record ClientOptions(DebuggerOptions? DebuggerOptions = null, bool UseVisualStudio = false);
+public sealed record ClientOptions(
+  DebuggerOptions? DebuggerOptions = null,
+  bool UseVisualStudio = false,
+  ExternalTerminalOptions? ExternalTerminal = null);
+
+public sealed record ExternalTerminalOptions(
+    string Command = "",
+    string[]? Args = null
+);
