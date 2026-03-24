0a33f2050dff6ec19e71e72ed3a3c65c5d787756
GustavEikaas <gustav.eikaas@gmail.com>
2026-03-24 15:17:08 +0100

    .

--- EasyDotnet.IDE/Workspace/Controllers/WorkspaceRunController.cs
+++ EasyDotnet.IDE/Workspace/Controllers/WorkspaceRunController.cs
@@ -7,6 +7,5 @@ namespace EasyDotnet.IDE.Workspace.Controllers;
 public class WorkspaceRunController(WorkspaceService service) : BaseController
 {
   [JsonRpcMethod("workspace/run", UseSingleObjectParameterDeserialization = true)]
-  public async Task RunAsync(WorkspaceRunRequest request, CancellationToken ct) =>
-    await service.RunAsync(request, ct);
+  public async Task RunAsync(WorkspaceRunRequest request, CancellationToken ct) => await service.RunAsync(request, ct);
 }
