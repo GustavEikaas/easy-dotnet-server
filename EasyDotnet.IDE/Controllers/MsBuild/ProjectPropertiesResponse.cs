b9f99dda1bfb368a4b10cab7af48fd486be97d3e
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2026-05-14 14:58:08 +0200

    chore: slim down DotnetProjectv1 (#411)

--- EasyDotnet.IDE/Controllers/MsBuild/ProjectPropertiesResponse.cs
+++ EasyDotnet.IDE/Controllers/MsBuild/ProjectPropertiesResponse.cs
@@ -6,75 +6,43 @@ namespace EasyDotnet.IDE.Controllers.MsBuild;
 public sealed record DotnetProjectV1(
     string ProjectName,
     string Language,
-    string? OutputPath,
     string? OutputType,
-    string? TargetExt,
-    string? AssemblyName,
     string? TargetFramework,
     string[]? TargetFrameworks,
     bool IsTestProject,
+    bool IsTestingPlatformApplication,
     bool IsWebProject,
     bool IsWorkerProject,
-    string? UserSecretsId,
-    bool TestingPlatformDotnetTestSupport,
-    bool IsTestingPlatformApplication,
     string? TargetPath,
     bool GeneratePackageOnBuild,
     bool IsPackable,
-    string? LangVersion,
-    string? RootNamespace,
-    string? PackageId,
-    string? NugetVersion,
     string? Version,
-    string? PackageOutputPath,
     bool IsMultiTarget,
-    bool IsNetFramework,
-    bool UseIISExpress,
-    bool IsAspireHost,
-    Version? AspireHostingSdkVersion
+    bool UseIISExpress
 );
 
 public static class DotnetProjectExtensions
 {
   public static DotnetProjectV1 ToResponse(this DotnetProject project)
   {
-    var nugetVersion = project.Version?.ToString();
-    var targetFrameworkVersion = project.TargetFrameworkVersion;
-    var isNetFramework = targetFrameworkVersion?.StartsWith("v4") == true;
-    var useIISExpress = project.UseIISExpress;
-    var targetPath = project.TargetPath;
     var projectName = project.MSBuildProjectName ?? throw new Exception("MSBuildProjectName can never be null");
-    var aspireSdkVersion = project.AspireHostingSDKVersion;
 
     return new DotnetProjectV1(
         ProjectName: projectName,
         Language: GetLanguage(project.MSBuildProjectFullPath ?? throw new Exception("MSBuildProjectName can never be null")),
-        OutputPath: project.OutputPath,
         OutputType: project.OutputType,
-        TargetExt: project.TargetExt,
-        AssemblyName: project.AssemblyName,
         TargetFramework: project.TargetFramework,
         TargetFrameworks: project.TargetFrameworks,
         IsTestProject: project.IsTestProject,
+        IsTestingPlatformApplication: project.IsTestingPlatformApplication,
         IsWebProject: project.UsingMicrosoftNETSdkWeb,
         IsWorkerProject: project.UsingMicrosoftNETSdkWorker,
-        UserSecretsId: project.UserSecretsId,
-        TestingPlatformDotnetTestSupport: project.TestingPlatformDotnetTestSupport,
-        IsTestingPlatformApplication: project.IsTestingPlatformApplication,
-        TargetPath: targetPath,
+        TargetPath: project.TargetPath,
         GeneratePackageOnBuild: project.GeneratePackageOnBuild,
         IsPackable: project.IsPackable,
-        LangVersion: project.LangVersion,
-        RootNamespace: project.RootNamespace,
-        PackageId: project.PackageId,
-        NugetVersion: string.IsNullOrWhiteSpace(nugetVersion) ? null : nugetVersion,
-        Version: targetFrameworkVersion,
-        PackageOutputPath: project.PackageOutputPath,
+        Version: project.TargetFrameworkVersion,
         IsMultiTarget: project.TargetFrameworks?.Length > 1,
-        IsNetFramework: isNetFramework,
-        UseIISExpress: useIISExpress,
-        IsAspireHost: project.IsAspireHost,
-        AspireHostingSdkVersion: aspireSdkVersion
+        UseIISExpress: project.UseIISExpress
     );
   }
 
