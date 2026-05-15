80f97f7358ddebeb464f896e07cad646c6fefde2
GustavEikaas <gustav.eikaas@gmail.com>
2026-05-15 08:24:32 +0200

    .

--- EasyDotnet.ContainerTests/Initialize/InitializeContainerTests.cs
+++ EasyDotnet.ContainerTests/Initialize/InitializeContainerTests.cs
@@ -1,6 +1,6 @@
+using System.Threading.Channels;
 using EasyDotnet.ContainerTests.Docker;
 using EasyDotnet.ContainerTests.Scaffold;
-using System.Threading.Channels;
 using StreamJsonRpc;
 
 namespace EasyDotnet.ContainerTests.Initialize;
