0e61fd6b1c2f2d6dd15bc3a299cf65ec132ac45f
GustavEikaas <gustav.eikaas@gmail.com>
2026-06-08 15:28:02 +0200

    ci: include dncdbg

--- EasyDotnet.IDE.Tests/Debugger/DebuggerLocatorTests.cs
+++ EasyDotnet.IDE.Tests/Debugger/DebuggerLocatorTests.cs
@@ -10,6 +10,7 @@ public class DebuggerLocatorTests
   public void GetDebuggerPath_UsesBundledVersion_WhenNoEnvVarSet()
   {
     Environment.SetEnvironmentVariable(NetCoreDbgLocator.DEBUGGER_PATH_ENV, null);
+    Environment.SetEnvironmentVariable(DebuggerLocator.DEBUGGER_ENGINE_ENV, null);
     Assert.Throws<FileNotFoundException>(() => NetCoreDbgLocator.GetNetCoreDbgPath());
   }
 
@@ -30,6 +31,100 @@ public class DebuggerLocatorTests
     }
   }
 
+  [Test]
+  public async Task ResolveDebugger_DefaultsToNetCoreDbg()
+  {
+    Environment.SetEnvironmentVariable(NetCoreDbgLocator.DEBUGGER_PATH_ENV, null);
+    Environment.SetEnvironmentVariable(DebuggerLocator.DEBUGGER_ENGINE_ENV, null);
+
+    var engine = DebuggerLocator.GetConfiguredEngine();
+
+    await Assert.That(engine).IsEqualTo(DebuggerEngine.NetCoreDbg);
+  }
+
+  [Test]
+  public async Task ResolveDebugger_UsesDncDbgEngine_WhenEnvVarSet()
+  {
+    Environment.SetEnvironmentVariable(NetCoreDbgLocator.DEBUGGER_PATH_ENV, null);
+    Environment.SetEnvironmentVariable(DebuggerLocator.DEBUGGER_ENGINE_ENV, "dncdbg");
+
+    var engine = DebuggerLocator.GetConfiguredEngine();
+
+    await Assert.That(engine).IsEqualTo(DebuggerEngine.DncDbg);
+  }
+
+  [Test]
+  public async Task ResolveDebugger_UsesCustomPath_WhenDncDbgEngineSet()
+  {
+    var customDebuggerPath = Path.GetTempFileName();
+    try
+    {
+      Environment.SetEnvironmentVariable(NetCoreDbgLocator.DEBUGGER_PATH_ENV, customDebuggerPath);
+      Environment.SetEnvironmentVariable(DebuggerLocator.DEBUGGER_ENGINE_ENV, "dncdbg");
+
+      var debugger = DebuggerLocator.ResolveDebugger();
+
+      await Assert.That(debugger.Engine).IsEqualTo(DebuggerEngine.DncDbg);
+      await Assert.That(debugger.Source).IsEqualTo(DebuggerLocator.DEBUGGER_PATH_ENV);
+      await Assert.That(debugger.Path).IsEqualTo(customDebuggerPath);
+    }
+    finally
+    {
+      File.Delete(customDebuggerPath);
+      Environment.SetEnvironmentVariable(NetCoreDbgLocator.DEBUGGER_PATH_ENV, null);
+      Environment.SetEnvironmentVariable(DebuggerLocator.DEBUGGER_ENGINE_ENV, null);
+    }
+  }
+
+  [Test]
+  public async Task ResolveDebugger_UsesBundledDncDbgPath_WhenEngineSet()
+  {
+    Environment.SetEnvironmentVariable(NetCoreDbgLocator.DEBUGGER_PATH_ENV, null);
+    Environment.SetEnvironmentVariable(DebuggerLocator.DEBUGGER_ENGINE_ENV, "dncdbg");
+
+    var platform = DebuggerLocator.GetRuntimePlatform();
+    var expectedPath = DebuggerLocator.GetBundledDebuggerPath(DebuggerEngine.DncDbg, platform);
+    var existed = File.Exists(expectedPath);
+
+    try
+    {
+      Directory.CreateDirectory(Path.GetDirectoryName(expectedPath)!);
+      if (!existed)
+      {
+        await File.WriteAllTextAsync(expectedPath, "");
+      }
+
+      var debugger = DebuggerLocator.ResolveDebugger();
+
+      await Assert.That(debugger.Engine).IsEqualTo(DebuggerEngine.DncDbg);
+      await Assert.That(debugger.Source).IsEqualTo("bundled");
+      await Assert.That(debugger.Platform).IsEqualTo(platform);
+      await Assert.That(debugger.Path).IsEqualTo(expectedPath);
+    }
+    finally
+    {
+      if (!existed && File.Exists(expectedPath))
+      {
+        File.Delete(expectedPath);
+      }
+
+      Environment.SetEnvironmentVariable(NetCoreDbgLocator.DEBUGGER_PATH_ENV, null);
+      Environment.SetEnvironmentVariable(DebuggerLocator.DEBUGGER_ENGINE_ENV, null);
+    }
+  }
+
+  [Test]
+  public void ResolveDebugger_ThrowsForInvalidEngine()
+  {
+    Environment.SetEnvironmentVariable(DebuggerLocator.DEBUGGER_ENGINE_ENV, "invalid");
+
+    Assert.Throws<ArgumentException>(() => DebuggerLocator.GetConfiguredEngine());
+  }
+
   [After(Test)]
-  public void Cleanup() => Environment.SetEnvironmentVariable(NetCoreDbgLocator.DEBUGGER_PATH_ENV, null);
+  public void Cleanup()
+  {
+    Environment.SetEnvironmentVariable(NetCoreDbgLocator.DEBUGGER_PATH_ENV, null);
+    Environment.SetEnvironmentVariable(DebuggerLocator.DEBUGGER_ENGINE_ENV, null);
+  }
 }
